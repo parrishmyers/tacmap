b99ed3543c1408e50f900e930893e4ccb560dfb5
Parrish Myers <parrishmyers@gmail.com>
2016-06-25 16:02:40 -0700

    Opps forgot this one

--- tacmap/DT_TriangleList.h
+++ tacmap/DT_TriangleList.h
@@ -9,18 +9,58 @@
 #ifndef DT_List_h
 #define DT_List_h
 
-template<typename T, int Size>
-class List {
-    T data[Size];
-    int size;
-    int len;
+#include <stdio.h>
+
+#include "DT_Triangle.h"
+
+template<int Size>
+class TriangleList {
+public:
+    Triangle * data[Size];
+    int size = Size;
+    int len = 0;
+
+    TriangleList<>() : size(Size), len(0) { }
     
-    List<>() : size(Size), len(0) { }
+    void copy(TriangleList<Size> & tl) {
+        clear();
+        for (int i = 0; i < tl.len; i++) {
+            add(tl[i]);
+        }
+    }
     
     void clear() {
-        for (int i = 0; i < size) {
-            data[i] = T();
+        for (int i = 0; i < size; i++) {
+            data[i] = nullptr;
         }
+        len = 0;
+    }
+    
+    bool inList(Triangle *a) {
+        bool result = false;
+        for (int i = 0; i < len; i++) {
+            if (a == data[i])
+                result = true;
+        }
+        return result;
+    }
+    
+    void add(Triangle * a) {
+        if (len < size) {
+            if (false == inList(a)) {
+                data[len] = a;
+                len += 1;
+            }
+        } else {
+            fprintf(stdout, "ERROR: TriangleList full\n");
+        }
+    }
+    
+    Triangle * operator[] (int i) {
+        if (0 <= i && i < size)
+            return data[i];
+        else
+            return nullptr;
     }
 };
 #endif /* DT_List_h */
